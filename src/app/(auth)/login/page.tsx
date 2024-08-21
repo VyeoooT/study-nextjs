a7f23e2a1626054aed6843e0f70f949f4118cc58
vyeo <vlinh52000@gmail.com>
2024-08-21 20:03:39 +0700

    - Login form; - Try catch API; - Add toast

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,10 @@
+import LoginForm from "@/app/(auth)/login/loginForm";
+
 export default function LoginPage() {
   return (
-    <div>
-      Login Page
+    <div className="flex flex-col items-center gap-10">
+      <h1 className="text-3xl font-bold">Form Login</h1>
+      <LoginForm />
     </div>
   )
 }
